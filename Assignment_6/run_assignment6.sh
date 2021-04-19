346e1ffcb08b64d99b8a99db1fd0419d8fe1949c
JohanneBW <70207251+JohanneBW@users.noreply.github.com>
2021-04-19 10:12:35 +0200

    Update run_assignment6.sh

--- Assignment_6/run_assignment6.sh
+++ Assignment_6/run_assignment6.sh
@@ -10,7 +10,13 @@ test -f requirements.txt && pip install requirements.txt
 
 echo "build $VENVNAME"
 
+#download and unzip pretraind embeddings
+cd data
+wget http://nlp.stanford.edu/data/glove.6B.zip
+unzip -q glove.6B.zip
+
 #run scripts
+cd ..
 python3 LRModel.py
 python3 DLModel.py
 
