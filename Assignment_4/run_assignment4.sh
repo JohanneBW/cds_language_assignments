a7c2bd714c3d1f6554c3d90653f55992d721fdd7
JohanneBW <70207251+JohanneBW@users.noreply.github.com>
2021-04-23 11:28:47 +0200

    Update and rename create_venv.sh to run_assignment4.sh

--- Assignment_4/run_assignment4.sh
+++ Assignment_4/run_assignment4.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
 
-VENVNAME=Assignment_W6_env
+VENVNAME=Assignment_4
 
 python3 -m venv $VENVNAME
 source $VENVNAME/bin/activate
 pip install --upgrade pip
 
-python -m ipykernel install --user --name=$VENVNAME
-
 test -f requirements.txt && pip install -r requirements.txt
+
+# run script
+python3 network.py
 
 deactivate
 echo "build $VENVNAME"
