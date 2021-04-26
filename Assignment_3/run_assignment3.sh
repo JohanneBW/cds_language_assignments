6a537dc710e296cbaeed78939bcdb74b498e78e3
JohanneBW <70207251+JohanneBW@users.noreply.github.com>
2021-04-26 16:14:56 +0200

    Add files via upload

---
+++ Assignment_3/run_assignment3.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+VENVNAME=LA_Assignment3_venv
+
+python3 -m venv $VENVNAME
+source $VENVNAME/bin/activate
+pip install --upgrade pip
+
+test -f requirements.txt && pip install requirements.txt
+
+#echo "build $VENVNAME"
+
+#download
+python3 -m spacy download en_core_web_sm
+
+#run script
+python3 sentiment.py
+
+#deactivate environment
+deactivate
