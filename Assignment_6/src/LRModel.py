5a35717e483ddfd04a76d63b657b015a5b776d0b
JohanneBW <70207251+JohanneBW@users.noreply.github.com>
2021-05-20 09:01:58 +0200

    Update and rename Assignment_6/LRModel.py to Assignment_6/src/LRModel.py

--- Assignment_6/src/LRModel.py
+++ Assignment_6/src/LRModel.py
@@ -40,61 +40,60 @@ from tensorflow.keras.regularizers import L2
 # matplotlib
 import matplotlib.pyplot as plt
 
+
+"""
+---------- Functions ----------
+"""    
+def plot_history(H, epochs):
+    """
+    Utility function for plotting model history using matplotlib
+    H: model history 
+    epochs: number of epochs for which the model was trained
+    """
+    plt.style.use("fivethirtyeight")
+    plt.figure()
+    plt.plot(np.arange(0, epochs), H.history["loss"], label="train_loss")
+    plt.plot(np.arange(0, epochs), H.history["val_loss"], label="val_loss")
+    plt.plot(np.arange(0, epochs), H.history["accuracy"], label="train_acc")
+    plt.plot(np.arange(0, epochs), H.history["val_accuracy"], label="val_acc")
+    plt.title("Training Loss and Accuracy")
+    plt.xlabel("Epoch #")
+    plt.ylabel("Loss/Accuracy")
+    plt.legend()
+    plt.tight_layout()
+    plt.show()
+    
+def create_embedding_matrix(filepath, word_index, embedding_dim):
+    """ 
+    A helper function to read in saved GloVe embeddings and create an embedding matrix
+
+    filepath: path to GloVe embedding
+    word_index: indices from keras Tokenizer
+    embedding_dim: dimensions of keras embedding layer
+    """
+    vocab_size = len(word_index) + 1  # Adding again 1 because of reserved 0 index
+    embedding_matrix = np.zeros((vocab_size, embedding_dim))
+
+    with open(filepath) as f:
+        for line in f:
+            word, *vector = line.split()
+            if word in word_index:
+                idx = word_index[word] 
+                embedding_matrix[idx] = np.array(
+                vector, dtype=np.float32)[:embedding_dim]
+    return embedding_matrix
+
 """
 ---------- Main function ----------
 """
 
 def main():
-
-    """
-    ---------- Functions ----------
-    """    
-    def plot_history(H, epochs):
-        """
-        Utility function for plotting model history using matplotlib
-        H: model history 
-        epochs: number of epochs for which the model was trained
-        """
-        plt.style.use("fivethirtyeight")
-        plt.figure()
-        plt.plot(np.arange(0, epochs), H.history["loss"], label="train_loss")
-        plt.plot(np.arange(0, epochs), H.history["val_loss"], label="val_loss")
-        plt.plot(np.arange(0, epochs), H.history["accuracy"], label="train_acc")
-        plt.plot(np.arange(0, epochs), H.history["val_accuracy"], label="val_acc")
-        plt.title("Training Loss and Accuracy")
-        plt.xlabel("Epoch #")
-        plt.ylabel("Loss/Accuracy")
-        plt.legend()
-        plt.tight_layout()
-        plt.show()
-    
-
-    def create_embedding_matrix(filepath, word_index, embedding_dim):
-        """ 
-        A helper function to read in saved GloVe embeddings and create an embedding matrix
-
-        filepath: path to GloVe embedding
-        word_index: indices from keras Tokenizer
-        embedding_dim: dimensions of keras embedding layer
-        """
-        vocab_size = len(word_index) + 1  # Adding again 1 because of reserved 0 index
-        embedding_matrix = np.zeros((vocab_size, embedding_dim))
-
-        with open(filepath) as f:
-            for line in f:
-                word, *vector = line.split()
-                if word in word_index:
-                    idx = word_index[word] 
-                    embedding_matrix[idx] = np.array(
-                        vector, dtype=np.float32)[:embedding_dim]
-
-        return embedding_matrix
-
+  
     """
     ---------- Read data ----------
     """
     # Read the data into a pandas data frame
-    filepath = "data/Game_of_Thrones_Script.csv"
+    filepath = os.path.join("..", "data", "Game_of_Thrones_Script.csv")
     df = pd.read_csv(filepath)
     # Make a df with the two columns: season and sentence from the original data set
     df = df[["Season", "Sentence"]]
@@ -144,7 +143,7 @@ def main():
     # Plot the learning curve
     clf.plot_learning_curve(model, title, X_vect, season, cv=cv, n_jobs=4)
     # Save image in output folder
-    plt.savefig("output/LR_CrossValidation.png")
+    plt.savefig("../output/LR_CrossValidation.png")
     
 #Define behaviour when called from command line
 if __name__ == "__main__":
