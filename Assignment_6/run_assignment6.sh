6dfbe8fc181da3a7a0fb7bbdb6654d22176eb524
JohanneBW <70207251+JohanneBW@users.noreply.github.com>
2021-05-21 09:41:14 +0200

    Update run_assignment6.sh

--- Assignment_6/run_assignment6.sh
+++ Assignment_6/run_assignment6.sh
@@ -6,7 +6,7 @@ python3 -m venv $VENVNAME
 source $VENVNAME/bin/activate
 pip install --upgrade pip
 
-test -f requirements.txt && pip install requirements.txt
+test -f requirements.txt && pip install -r requirements.txt
 
 echo "build $VENVNAME"
 
